e65a5259e809605ffd56b088bc1118132848d801
GabrielNkwa <gnkwa@outlook.com>
2022-11-11 11:13:43 +0100

    minor tweaks

---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+ <nav id="navbarExample" class="navbar navbar-expand-lg fixed-top navbar-light" aria-label="Main navigation">
+  <div class="container">
+       <a class="navbar-brand logo-text" href="#">Panzy Pips</a> 
+      <button class="navbar-toggler p-0 border-0" type="button" id="navbarSideCollapse" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav ms-auto navbar-nav-scroll">
+              <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#header">Home</a>
+              </li>
+              
+              <li class="nav-item">
+                  <a class="nav-link" href="#details">Courses</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Analysis</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Events</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Forex Signal</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Market Direction</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Pips Calculator</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">About</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Contact</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Blog</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#features">Login</a>
+              </li>
+              
+          </ul>
+          <span class="nav-item">
+              <a class="btn-solid-sm" href="#contact">Contact Us</a>
+          </span>
+      </div> 
+  </div> 
+</nav>
